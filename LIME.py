43e19a6b4f307fbeea4097cab0e34cd918146b33
fravarcas <xxraptor51esp@gmail.com>
2023-05-31 10:56:25 +0200

    feat:Instalada libreria LIME
    Se ha instalado la librería LIME y se ha comenzado
    con la implementación del algoritmo.

--- LIME.py
+++ LIME.py
@@ -1,3 +1,10 @@
 import numpy as np
 import scipy as sc
+import lime
+from lime import lime_tabular
+
+def LIMEAlgorithm(training_data, column_names, discretize_continuous):
+
+    explaining_alg = lime_tabular.LimeTabularExplainer(training_data, column_names, discretize_continuous)
+
 
