0760d57a11fddd481d6fe0e36fdba3ce1a4acb78
fravarcas <xxraptor51esp@gmail.com>
2023-06-01 11:50:34 +0200

    feat:Comienzo de implementación LIME

--- LIME.py
+++ LIME.py
@@ -1,10 +1,18 @@
 import numpy as np
 import scipy as sc
-import lime
-from lime import lime_tabular
 
-def LIMEAlgorithm(training_data, column_names, discretize_continuous):
 
-    explaining_alg = lime_tabular.LimeTabularExplainer(training_data, column_names, discretize_continuous)
+def LIMEAlgorithm(training_data, perm_number):
+    X = []
+    R = []
+    W = []
+    for i in range (1, perm_number):
+
+        atributes = np.random.choice(np.random.randint(1, training_data.size()),np.random.randint(1, training_data.size()))
+        for atribute in atributes:
+            atributeToModify = training_data[atribute]
+            modifiedAtribute = 
+            W.append(abs(modifiedAtribute - atributeToModify))
+            X.append(modifiedAtribute)
 
 
